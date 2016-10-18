841f575dbe9b186c4159ae0e46d221c79ae0e2e1
Colin Moore <colinmoore716@gmail.com>
2016-10-18 14:19:09 -0400

    Changed color filter to use HSV
    The color filter now uses HSV ranges as opposed to RGB ranges to
    identify the targets.

--- filters.py
+++ filters.py
@@ -3,40 +3,38 @@ import numpy as np
 
 def filter_1(image):
     imgs = []
-    #Otsu adaptive threshold
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    _, mask = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
-    #Erode and dialate to remove small noise 
+    #Color Threshold
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    
+    red_mask = cv2.inRange(image_hsv, np.array([0, 125, 150]), np.array([360, 150, 255]))
+    blue_mask = cv2.inRange(image_hsv, np.array([150, 100, 100]), np.array([200, 230, 230]))
+    yellow_mask = cv2.inRange(image_hsv, np.array([0, 125, 125]), np.array([40, 255, 255]))
+    green_mask = cv2.inRange(image_hsv, np.array([60, 50, 100]), np.array([120, 100, 255]))
+    
+    mask = cv2.bitwise_or(red_mask, blue_mask)
+    mask = cv2.bitwise_or(mask, yellow_mask)    
+    mask = cv2.bitwise_or(mask, green_mask)    
+    
     kernel = np.ones((3,3), np.uint8)
-    mask = cv2.erode(mask, kernel, iterations = 6)
-    mask = cv2.dilate(mask, kernel, iterations = 2)
-    res = cv2.bitwise_and(image, image, mask=mask)
-
-    #RGB Threshold
-    mask = cv2.inRange(res, np.array([0,0,0]), np.array([80, 255, 150]))
-    mask = cv2.bitwise_not(mask, mask)
-
-    mask = cv2.erode(mask, kernel, iterations = 2)
-    mask = cv2.dilate(mask, kernel, iterations = 8)
+    mask = cv2.erode(mask, kernel, iterations = 1)
+    mask = cv2.dilate(mask, kernel, iterations = 12)
     res = cv2.bitwise_and(image, image, mask=mask)
 
     canny = cv2.Canny(mask, 100, 200)
     im2, contours, hierarchy = cv2.findContours(canny,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     
-    contour_draw = np.zeros(image.shape)
+#    contour_draw = np.zeros(image.shape)
     for cnt in contours:
-        if (cv2.contourArea(cnt) > 100) and (cv2.contourArea(cnt) < 2000):
+        if (cv2.contourArea(cnt) > 100) and (cv2.contourArea(cnt) < 4000):
             x, y, width, height = cv2.boundingRect(cnt)
             roi = image[y:y+height, x:x+width]
             resize_dimensions = (500, int(height * (500/width)))
             roi = cv2.resize(roi, resize_dimensions, cv2.INTER_NEAREST)
-            #imgs.append(roi)
-            cv2.drawContours(contour_draw, [cnt], 0, (255, 255, 255), -1)
-    
-    mask = cv2.inRange(contour_draw, np.array([100, 100, 100]), np.array([255, 255, 255]))
-    res = cv2.bitwise_and(image, image, mask=mask) 
-    
-    #return imgs
-    return [res]
+            imgs.append(roi)
+#            cv2.drawContours(contour_draw, [cnt], 0, (255, 255, 255), -1)
+#
+#    mask = cv2.inRange(contour_draw, np.array([100, 100, 100]), np.array([255, 255, 255]))
+#    res = cv2.bitwise_and(image, image, mask=mask) 
+#    res = cv2.addWeighted(res,0.8,image,0.2,0)
+#    return [res]
+    return imgs
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import ARC
 import cv2
 import numpy as np
 from filters import *
+import time
 
 cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('Display', 1920, 1080)
@@ -11,18 +12,22 @@ images = []
 for tgt in targets:
     if not ((tgt.target_type == 0) or (tgt.target_type == 1)):
         continue
-    range = 10
+    dist = 10
     while True:
-        new_images = flight.images_near(tgt.coord, range)
+        new_images = flight.images_near(tgt.coord, dist)
         if new_images:
             break
-        range += 1
+        dist += 1
     images.extend(new_images)
 
 for image_file in images:
     filename = image_file.filename
     image = cv2.imread(filename[:-3] + 'jpg')
     imgs = filter_1(image)
+
+    cv2.imshow('Display', image)
+    cv2.waitKey()
+
     for img in imgs:
         cv2.imshow('Display', img)
         cv2.waitKey()
