150a79a3f75e0f65c270df0086012bb911d3b960
Colin Moore <camoore7@ncsu.edu>
2016-10-21 19:27:05 -0400

    Added secondary filter to check ROI's for targets
    check_target() in filters.py uses the roi's size, aspect ratio, and
    pixel density to determine likely targets.

--- filters.py
+++ filters.py
@@ -1,11 +1,43 @@
 import cv2
 import numpy as np
 
-def filter_1(image):
+def filter_primary(image):
     imgs = []
-    #Color Threshold
-    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     
+    mask = color_mask(image)
+
+    kernel = np.ones((3,3), np.uint8)
+    mask = cv2.erode(mask, kernel, iterations = 1)
+    mask = cv2.dilate(mask, kernel, iterations = 12)
+
+    canny = cv2.Canny(mask, 100, 200)
+    im2, contours, hierarchy = cv2.findContours(canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        if (cv2.contourArea(cnt) > 100) and (cv2.contourArea(cnt) < 4000):
+            x, y, width, height = cv2.boundingRect(cnt)
+            roi = image[y:y+height, x:x+width]
+            imgs.append(roi)
+    return imgs
+
+def check_target(image): 
+    kernel = np.ones((3,3), np.uint8)
+    mask = color_mask(image)
+    mask = cv2.erode(mask, kernel, iterations = 1)
+    
+    res = cv2.bitwise_and(image, image, mask=mask)
+    x, y, w, h = cv2.boundingRect(mask)
+    if h == 0:
+        return False
+    ar = (float(w)/h)
+    m = cv2.moments(mask)
+    if(cv2.contourArea(np.array([(x,y), (x,y+h), (x+w,y+h), (x+w,y)])) > 20 ) and (ar > 0.65 and ar < 1.54) and (m['m00'] > 5000):
+        return True
+    return False
+
+def color_mask(image):
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
     red_mask = cv2.inRange(image_hsv, np.array([0, 125, 150]), np.array([360, 150, 255]))
     blue_mask = cv2.inRange(image_hsv, np.array([150, 100, 100]), np.array([200, 230, 230]))
     yellow_mask = cv2.inRange(image_hsv, np.array([0, 125, 125]), np.array([40, 255, 255]))
@@ -13,28 +45,5 @@ def filter_1(image):
     
     mask = cv2.bitwise_or(red_mask, blue_mask)
     mask = cv2.bitwise_or(mask, yellow_mask)    
-    mask = cv2.bitwise_or(mask, green_mask)    
-    
-    kernel = np.ones((3,3), np.uint8)
-    mask = cv2.erode(mask, kernel, iterations = 1)
-    mask = cv2.dilate(mask, kernel, iterations = 12)
-    res = cv2.bitwise_and(image, image, mask=mask)
-
-    canny = cv2.Canny(mask, 100, 200)
-    im2, contours, hierarchy = cv2.findContours(canny,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    
-#    contour_draw = np.zeros(image.shape)
-    for cnt in contours:
-        if (cv2.contourArea(cnt) > 100) and (cv2.contourArea(cnt) < 4000):
-            x, y, width, height = cv2.boundingRect(cnt)
-            roi = image[y:y+height, x:x+width]
-            resize_dimensions = (500, int(height * (500/width)))
-            roi = cv2.resize(roi, resize_dimensions, cv2.INTER_NEAREST)
-            imgs.append(roi)
-#            cv2.drawContours(contour_draw, [cnt], 0, (255, 255, 255), -1)
-#
-#    mask = cv2.inRange(contour_draw, np.array([100, 100, 100]), np.array([255, 255, 255]))
-#    res = cv2.bitwise_and(image, image, mask=mask) 
-#    res = cv2.addWeighted(res,0.8,image,0.2,0)
-#    return [res]
-    return imgs
+    mask = cv2.bitwise_or(mask, green_mask)
+    return mask
--- test.py
+++ test.py
@@ -6,28 +6,37 @@ import time
 
 cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('Display', 1920, 1080)
+
 flight = ARC.Flight(157)
-targets = flight.all_targets()
-images = []
-for tgt in targets:
-    if not ((tgt.target_type == 0) or (tgt.target_type == 1)):
-        continue
-    dist = 10
-    while True:
-        new_images = flight.images_near(tgt.coord, dist)
-        if new_images:
-            break
-        dist += 1
-    images.extend(new_images)
+
+#targets = flight.all_targets()
+#images = []
+#
+#for tgt in targets:
+#    if not ((tgt.target_type == 0) or (tgt.target_type == 1)):
+#        continue
+#    dist = 10
+#    while True:
+#        new_images = flight.images_near(tgt.coord, dist)
+#        if new_images:
+#            break
+#        dist += 1
+#    images.extend(new_images)
+
+images = flight.all_images()
+
 
 for image_file in images:
+#for i in range(1):
+#    image_file = images[0]
     filename = image_file.filename
     image = cv2.imread(filename[:-3] + 'jpg')
-    imgs = filter_1(image)
+    imgs = filter_primary(image)
 
-    cv2.imshow('Display', image)
-    cv2.waitKey()
 
     for img in imgs:
-        cv2.imshow('Display', img)
-        cv2.waitKey()
+        if(check_target(img)):
+            cv2.imshow('Display', image)
+            cv2.waitKey()
+            cv2.imshow('Display', img)
+            cv2.waitKey()
