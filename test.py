592f76753766aea1a56f06bef910f21505b8f4e4
Colin Moore <colinmoore716@gmail.com>
2017-03-04 21:13:42 -0500

    Added ROI Test

--- test.py
+++ test.py
@@ -1,38 +1,58 @@
+import unittest
+import time
+
 import ARC
-import cv2
-import numpy as np
-import argparse
 import filters
+import cv2
 
-cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Display', 1920, 1080)
+class FilterTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.flight = ARC.Flight(165)
+        self.targets = self.flight.all_targets()
+        self.target_images = []
+        
+        for tgt in self.targets:
+            if not ((tgt.target_type == 0) or (tgt.target_type == 1) or (tgt.target_type == None)):
+                continue
+            new_images = self.flight.images_near(tgt.coord, 50)
+            self.target_images.extend(new_images)
+        #Remove duplicate files
+        self.target_images = dict((image.filename, image) for image in self.target_images).values()
+        self.images = self.flight.all_images()
+        
+    def test_get_contours(self):
+        for img, i in zip(self.target_images, range(len(self.target_images))):
+            with self.subTest(i=i):
+                start_time = time.time()
+                rois = filters.get_contours(cv2.imread(img.high_quality_jpg), goal=300)
+                self.assertLess((time.time()-start_time), 2) # Ensure the operation took less than 2 seconds
+                self.assertLess(len(rois), 330)
+                self.assertGreater(len(rois), 270)
+    
+    def test_roi(self):
+        cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Display', 1920, 1080)
+        for img in self.target_images:
+            rois = filters.high_pass_filter(img)
+            for roi in rois:
+                cv2.imshow('Display', roi.roi)
+                cv2.waitKey()
 
-parser = argparse.ArgumentParser(description='Search flight images for targets.')
-parser.add_argument("-i", "--input", required=True, help="Flight number to search")
-args = vars(parser.parse_args())
+            
+    def test_get_contours_canny(self):
+        cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Display', 1920, 1080)
+        for img in self.target_images:
+            (rois, canny) = filters.get_contours(cv2.imread(img.high_quality_jpg), goal=300, getCanny=True)
+            cv2.imshow('Display', canny)
+            cv2.waitKey()
 
-flight = ARC.Flight(args['input'])
-targets = flight.all_targets()
-images = []
+    def test_high_pass_filter(self):
+        return
 
-for tgt in targets:
-    if not ((tgt.target_type == 0) or (tgt.target_type == 1) or (tgt.target_type == None)):
-        continue
-    new_images = flight.images_near(tgt.coord, 50)
-    images.extend(new_images)
-#Remove duplicate files
-images = dict((image.filename, image) for image in images).values()
-#images = flight.all_images()
-
-n = 0
-
-for image_file in images:
-    filename = image_file.filename
-    image = cv2.imread(filename[:-3] + 'jpg')
-    ROIs = filters.high_pass_filter(image_file)
-    ROIs = filters.false_positive_filter(ROIs)
-    for roi in ROIs:
-        cv2.imshow('Display', roi.roi)
-        cv2.waitKey()
-        #cv2.imwrite("roi/roi{}.jpg".format(n), roi.roi)
-        n += 1
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    suite.addTest(FilterTest("test_get_contours_canny"))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
