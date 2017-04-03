9277ecbdb9748e1cf554eedc058ab42b76314ebb
Colin Moore <colinmoore716@gmail.com>
2017-04-03 14:23:28 -0400

    Added test for get target info

--- test.py
+++ test.py
@@ -13,10 +13,12 @@ class FilterTest(unittest.TestCase):
         cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
         cv2.resizeWindow('Display', 1920, 1080)
 
-        self.flight = ARC.Flight(165)
+        self.flight = ARC.Flight(167)
         self.targets = self.flight.all_targets()
         self.target_images = []
-        
+        if self.targets == None:
+            self.targets = []
+
         for tgt in self.targets:
             if not ((tgt.target_type == 0) or (tgt.target_type == 1) or (tgt.target_type == None)):
                 continue
@@ -30,15 +32,15 @@ class FilterTest(unittest.TestCase):
         for img, i in zip(self.target_images, range(len(self.target_images))):
             with self.subTest(i=i):
                 start_time = time.time()
-                rois = filters.get_contours(cv2.imread(img.high_quality_jpg), goal=300)
+                contours = filters.get_contours(cv2.imread(img.high_quality_jpg), goal=300)
                 self.assertLess((time.time()-start_time), 2) # Ensure the operation took less than 2 seconds
                 self.assertLess(len(rois), 330)
                 self.assertGreater(len(rois), 270)
     
     def test_get_contours_canny(self):
-        for img in self.target_images:
-            image = cv2.imread(img)
-            (contours, canny) = filters.get_contours(image, goal=600, getCanny=True)
+        for img in self.images:
+            image = cv2.imread(img.high_quality_jpg)
+            (contours, canny) = filters.get_contours(image, goal=300, getCanny=True)
             canny = cv2.cvtColor(canny, cv2.COLOR_GRAY2RGB)
 
             res = cv2.addWeighted(image, 0.6, canny, 0.4, 0)
@@ -46,13 +48,12 @@ class FilterTest(unittest.TestCase):
             cv2.waitKey()
 
     def test_get_rois(self):
-        for img in self.target_images:
+        for img in self.images:
             print('Testing')
             rois = filters.get_rois(img)
             for roi in rois:
                 cv2.imshow('Display', roi.roi)
                 cv2.waitKey()
-        return
 
     def test_get_targets(self):
         for img in self.target_images:
@@ -62,9 +63,79 @@ class FilterTest(unittest.TestCase):
                 cv2.waitKey()
                 cv2.imshow('Display', roi.roi_original)
                 cv2.waitKey()
+    
+    def test_get_target_info(self):
+        #Test a red target with a blue letter
+        redblue = cv2.imread('test_images/redblue.png')
+        redblue = cv2.cvtColor(redblue, cv2.COLOR_BGR2RGB)
+        
+        shape, letter = filters.get_target_info(redblue)
+        shape_color = roi.descale_color_value(shape[1])
 
+        self.assertLessEqual(shape_color[0], 255)
+        self.assertGreaterEqual(shape_color[0], 250)
+        self.assertLessEqual(shape_color[1], 5)
+        self.assertGreaterEqual(shape_color[1], 0)
+        self.assertLessEqual(shape_color[2], 5)
+        self.assertGreaterEqual(shape_color[2], 0)
+        
+        letter_color = roi.descale_color_value(letter[1])
+
+        self.assertLessEqual(letter_color[0], 5)
+        self.assertGreaterEqual(letter_color[0], 0)
+        self.assertLessEqual(letter_color[1], 5)
+        self.assertGreaterEqual(letter_color[1], 0)
+        self.assertLessEqual(letter_color[2], 255)
+        self.assertGreaterEqual(letter_color[2], 250)
+        
+        #Test a black target with a blue letter
+        blackblue = cv2.imread('test_images/blackblue.png')
+        blackblue = cv2.cvtColor(blackblue, cv2.COLOR_BGR2RGB)
+        
+        shape, letter = filters.get_target_info(blackblue)
+        shape_color = roi.descale_color_value(shape[1])
+
+        self.assertLessEqual(shape_color[0], 5)
+        self.assertGreaterEqual(shape_color[0], 0)
+        self.assertLessEqual(shape_color[1], 5)
+        self.assertGreaterEqual(shape_color[1], 0)
+        self.assertLessEqual(shape_color[2], 5)
+        self.assertGreaterEqual(shape_color[2], 0)
+        
+        letter_color = roi.descale_color_value(letter[1])
+
+        self.assertLessEqual(letter_color[0], 5)
+        self.assertGreaterEqual(letter_color[0], 0)
+        self.assertLessEqual(letter_color[1], 5)
+        self.assertGreaterEqual(letter_color[1], 0)
+        self.assertLessEqual(letter_color[2], 255)
+        self.assertGreaterEqual(letter_color[2], 250)
+        
+        #Test a red target with a black letter
+        redblack = cv2.imread('test_images/redblack.png')
+        redblack = cv2.cvtColor(redblack, cv2.COLOR_BGR2RGB)
+        
+        shape, letter = filters.get_target_info(redblack)
+        shape_color = roi.descale_color_value(shape[1])
+
+        self.assertLessEqual(shape_color[0], 255)
+        self.assertGreaterEqual(shape_color[0], 250)
+        self.assertLessEqual(shape_color[1], 5)
+        self.assertGreaterEqual(shape_color[1], 0)
+        self.assertLessEqual(shape_color[2], 5)
+        self.assertGreaterEqual(shape_color[2], 0)
+        
+        letter_color = roi.descale_color_value(letter[1])
+
+        self.assertLessEqual(letter_color[0], 5)
+        self.assertGreaterEqual(letter_color[0], 0)
+        self.assertLessEqual(letter_color[1], 5)
+        self.assertGreaterEqual(letter_color[1], 0)
+        self.assertLessEqual(letter_color[2], 5)
+        self.assertGreaterEqual(letter_color[2], 0)
+        
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    suite.addTest(FilterTest("test_get_targets"))
+    suite.addTest(FilterTest("test_get_target_info"))
     runner = unittest.TextTestRunner()
     runner.run(suite)
